d48e9632d4c34633ad4f2176e636855271fbe170
Santiago Valdivia <paulsantiagovaldivia@gmail.com>
2023-01-14 12:40:05 -0300

    create delate letter

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import { fromEvent, Subject } from 'rxjs';
-import WORDS_LIST from './wordsList.json'
+import { fromEvent, Subject} from "rxjs";
+import WORDS_LIST from "./wordsList.json";
 
-const letterRows = document.getElementsByClassName('letter-row');
+
+const letterRows = document.getElementsByClassName("letter-row");
 const onKeyDown$ = fromEvent(document, "keydown");
-let letterIndex=0;
-let letterRowIndex=0;
-let userAnswer=[];
-const getRandomWord=() => WORDS_LIST[Math.floor(Math.random()* WORDS_LIST.length)]
+let letterIndex = 0;
+let letterRowIndex = 0;
+let userAnswer = [];
+const getRandomWord = () =>
+  WORDS_LIST[Math.floor(Math.random() * WORDS_LIST.length)];
 let rigthWord = getRandomWord();
 console.log(rigthWord);
 
@@ -14,9 +16,10 @@ const userWindOrLoose$ = new Subject();
 
 const insertLetter = {
   next: (event) => {
-    const pressedKey = event.key;  
-    if(pressedKey.length == 1 && pressedKey.match(/[a-z]/i)){
-      let letterBox = Array.from(letterRows)[letterRowIndex].children[letterIndex];
+    const pressedKey = event.key;
+    if (pressedKey.length == 1 && pressedKey.match(/[a-z]/i)) {
+      let letterBox =
+        Array.from(letterRows)[letterRowIndex].children[letterIndex];
       letterBox.textContent = pressedKey;
       letterBox.classList.add("filled-letter");
       userAnswer.push(pressedKey);
@@ -24,21 +27,37 @@ const insertLetter = {
     }
   },
 };
-
-const checkWorld ={
-  next: (event)=>{
-    if(event.key =="Enter"){
-      if(userAnswer.join("") == rigthWord){
+const checkWorld = {
+  next: (event) => {
+    if (event.key == "Enter") {
+      if (userAnswer.join("") == rigthWord) {
         userWindOrLoose$.next("win");
       }
     }
-    
-  }
-}
+  },
+};
+
+const delateLetter = {
+  next: (event) => {
+    const pressedKey = event.key;
+    if (pressedKey.length == 0 && pressedKey == 'Backspace') {
+      let currentRow = letterRows[letterRowIndex];
+      let letterBox = currentRow.children[letterIndex-1]
+      letterBox.textContent="";
+      letterBox.classList.remove('filled-letter');
+      letterIndex--;
+      userAnswer.pop();
+    }
+  },
+};
 
 onKeyDown$.subscribe(insertLetter);
 onKeyDown$.subscribe(checkWorld);
-userWindOrLoose$.subscribe((value) =>{
+onKeyDown$.subscribe(delateLetter);
+userWindOrLoose$.subscribe(() => {
   let letterRowsWinned = Array.from(letterRows)[letterRowIndex];
-      console.log(letterRowsWinned);
-})
+  console.log(letterRowsWinned);
+  for (let i = 0; i < 5; i++) {
+    letterRowsWinned.children[i].classList.add("letter-green");
+  }
+});
