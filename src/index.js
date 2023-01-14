a2d6cdb1186d51f5ab71123ab3cc2796d869fcf9
Santiago Valdivia <paulsantiagovaldivia@gmail.com>
2023-01-14 11:10:16 -0300

    create keyboard listener

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
-import {fromEvent} from 'rxjs';
-const onKeyDown$ =fromEvent (document, "keydown");
-
-const observadorMouse={
-  next: (event) =>{
-    console.log(event.key);
-  }
-}; 
-  onKeyDown$.subscribe(observadorMouse);
+import { fromEvent } from "rxjs";
+const letterRows = document.getElementsByClassName('letter-row');
+const onKeyDown$ = fromEvent(document, "keydown");
+let letterIndex=0;
+let letterRowIndex=0;
+const insertLetter = {
+  next: (event) => {
+    const pressedKey = event.key;
+    if(pressedKey.length == 1 && pressedKey.match(/[a-z]/i)){
+      console.log(pressedKey);
+      console.log(letterRows);
+      let letterBox = Array.from(letterRows)[letterRowIndex].children[letterIndex];
+      letterBox.textContent = pressedKey;
+      letterBox.classList.add("filled-letter");
+      letterIndex++;
+    }
+  },
+};
+onKeyDown$.subscribe(insertLetter);
