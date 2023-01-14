66d11f0de817997f723b2ce9d5abd2315cda56af
Santiago Valdivia <paulsantiagovaldivia@gmail.com>
2023-01-14 08:02:32 -0300

    create an observer method

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import {Observable} from 'rxjs';
+const observableAlfa$= new Observable(suscriber =>{
+  suscriber.next(1);
+  suscriber.next(2);
+  suscriber.next(3);
+});
+
+const observador ={
+  next:(value) => {
+    console.log(value);
+  },
+  complete:() => {},
+  error:(error)=>{}
+};
+
+observableAlfa$.subscribe(observador);
