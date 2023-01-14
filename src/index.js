7200f7270c997be7eff42a707642fd6bc96a2585
Santiago Valdivia <paulsantiagovaldivia@gmail.com>
2023-01-14 11:20:42 -0300

    add json

--- src/index.js
+++ src/index.js
@@ -3,17 +3,26 @@ const letterRows = document.getElementsByClassName('letter-row');
 const onKeyDown$ = fromEvent(document, "keydown");
 let letterIndex=0;
 let letterRowIndex=0;
+let userAnswer=[];
 const insertLetter = {
   next: (event) => {
-    const pressedKey = event.key;
+    const pressedKey = event.key;  
     if(pressedKey.length == 1 && pressedKey.match(/[a-z]/i)){
-      console.log(pressedKey);
-      console.log(letterRows);
       let letterBox = Array.from(letterRows)[letterRowIndex].children[letterIndex];
       letterBox.textContent = pressedKey;
       letterBox.classList.add("filled-letter");
+      userAnswer.push(pressedKey);
       letterIndex++;
     }
   },
 };
+
+const checkWorld ={
+  next: (event)=>{
+    event.key? 'Enter':false;
+    
+  }
+}
+
 onKeyDown$.subscribe(insertLetter);
+onKeyDown$.subscribe(checkWorld);
